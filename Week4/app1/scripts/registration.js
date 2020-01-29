bab195257021b0b81b7d0149b50433b1f42d2178
Gerald Aden <gerald@gmatech.com>
2020-01-29 10:06:08 -0800

    Add week 4.

---
+++ Week4/app1/scripts/registration.js
@@ -0,0 +1,21 @@
+
+function validate() {
+    
+    document.getElementById('first-name-error').style.visibility = 'hidden';
+    document.getElementById('last-name-error').style.visibility = 'hidden';
+
+    let firstName = document.getElementById('first-name').value;
+    let lastName = document.getElementById('last-name').value;
+
+    if (firstName == '') {
+        document.getElementById('first-name-error').style.visibility = 'visible';
+        return;
+    }
+
+    if (lastName == '') {
+        document.getElementById('last-name-error').style.visibility = 'visible';
+        return;
+    }
+
+    window.location = 'index.html';
+}
---
+++ Week4/app1/scripts/table.js
@@ -0,0 +1,21 @@
+var persons = '{"persons": [{"firstName": "Tom", "lastName": "Smith"}, {"firstName": "Mary", "lastName": "Jones"}]}';
+
+function initializePersons() {
+    var data = getPersons();
+    displayPersons(data.persons);
+}
+
+function getPersons() {
+    return JSON.parse(persons);
+}
+
+function displayPersons(persons) {
+    var tableList = "";
+    for(i = 0; i < persons.length; i++) {
+        var firstName = persons[i].firstName;
+        var lastName = persons[i].lastName;
+        tableList += "<tr><td>" + firstName + "</td><td>" + lastName + "</td>";
+
+        document.getElementById("person-list").innerHTML = tableList;
+    }
+}
