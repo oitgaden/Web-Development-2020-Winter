7533f048ee996306289a5eac272f70d6996d0789
Gerald Aden <gerald@gmatech.com>
2020-02-22 13:28:28 -0800

    Corrected column name.

--- Week7/database/student-data.sql
+++ Week7/database/student-data.sql
@@ -1,2 +1,2 @@
-insert into student (email_address, person_id) values('john.smith@oit.edu', 9);
-insert into student (email_address, person_id) values('mary.jones@oit.edu', 10);
+insert into student (email_address, student_id) values('john.smith@oit.edu', 9);
+insert into student (email_address, student_id) values('mary.jones@oit.edu', 10);
